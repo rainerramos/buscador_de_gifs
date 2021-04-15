ed0d110af51090e7d9e1cf23c8588816b76e9fcd
Rainer Ramos <rainermr89@hotmail.com>
2021-04-14 22:37:15 -0300

    Iniciando o Layout e Criando um Indicador de Progresso

--- buscador_de_gifs/lib/main.dart
+++ buscador_de_gifs/lib/main.dart
@@ -5,6 +5,18 @@ import 'package:buscador_de_gifs/ui/home_page.dart';
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+  theme: ThemeData(
+  hintColor: Colors.white,
+  primaryColor: Colors.white,
+  inputDecorationTheme: InputDecorationTheme(
+  enabledBorder:
+  OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+  focusedBorder:
+  OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+  hintStyle: TextStyle(color: Colors.white),
+  )),
   ));
 }
+
+
 
--- buscador_de_gifs/lib/ui/home_page.dart
+++ buscador_de_gifs/lib/ui/home_page.dart
@@ -10,7 +10,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   String _search;
 
   int _offset = 0;
@@ -18,10 +17,12 @@ class _HomePageState extends State<HomePage> {
   Future<Map> _getGifs() async {
     http.Response response;
 
-    if(_search == null)
-      response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=GbIVRG4TBpIUD9KVQF23ESYBcT8d6eDk&limit=20&rating=g");
+    if (_search == null)
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=GbIVRG4TBpIUD9KVQF23ESYBcT8d6eDk&limit=20&rating=g");
     else
-      response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=GbIVRG4TBpIUD9KVQF23ESYBcT8d6eDk&$_search=dogs&limit=20&offset=$_offset&rating=g&lang=en");
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=GbIVRG4TBpIUD9KVQF23ESYBcT8d6eDk&$_search=dogs&limit=20&offset=$_offset&rating=g&lang=en");
 
     return json.decode(response.body);
   }
@@ -29,14 +30,65 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    
-    _getGifs().then((map){
+
+    _getGifs().then((map) {
       print(map);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network("https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget> [
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  labelText: "Pesquise Aqui",
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder()
+              ),
+              style: TextStyle(color: Colors.white, fontSize: 18.0),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: (context, snapshot){
+                switch(snapshot.connectionState){
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      width: 200.0,
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5.0,
+                      ),
+                    );
+                  default:
+                    if(snapshot.hasError) return Container();
+                    else  return _createGifTable(context, snapshot);
+                }
+              }
+            ),
+          ),
+        ],
+      ),
+    );
   }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+
+  }
+
 }
